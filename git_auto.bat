8f35d0f18f09dc6ad97b58862ac014a7f8f994df
yu47 <yummys47@163.com>
2024-02-21 09:45:44 +0800

    2024/02/21 周三

--- git_auto.bat
+++ git_auto.bat
@@ -2,5 +2,4 @@ cd E:\yu\Desktop\note\work_note
 E:
 git add .
 git commit -m "%date% "
-git push
-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
+echo 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 | git push
